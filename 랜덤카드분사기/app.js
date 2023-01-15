3a4916e0b115730057bb4319a4f56308147c71c5
KYW <qodna25@gmail.com>
2023-01-15 11:21:23 +0900

    랜덤카드분사기2

--- 랜덤카드분사기/app.js
+++ 랜덤카드분사기/app.js
@@ -1,43 +1,86 @@
 const random = document.querySelector(".random");
+const random2 = document.querySelector(".random2");
+const random3 = document.querySelector(".random3");
 const setting = document.querySelector(".setting");
-const body = document.querySelector('body')
+const $button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
 const cards = document.querySelectorAll(".card");
 
+let pageNumber = 0;
 
-function resize(){
+function pageNumCheck() {
+  $button.forEach((btnEl, i) => {
+    btnEl.addEventListener("click", () => {
+      pageNumber = i;
+      pageNumFocus();
+      cardRandom();
+    });
+  });
 
-    cardSetting()
+  function pageNumFocus() {
+    for (let i = 0; i < $button.length; i++) {
+      $button[i].classList.remove("active");
+    }
+    $button[pageNumber].classList.add("active");
+  }
 }
-
+pageNumCheck();
 
 function cardSetting() {
   cards.forEach((cardEl, i) => {
-    gsap.to(cardEl,
-         {
-            x: window.innerWidth/2-130 +i*-10 ,
-            y: window.innerHeight/2-300 +i*20 ,
-            ease:'back.out',
-            scale:0.5
-         });
-  });
-}
-
-function cardRandom() {
-  cards.forEach((cardEl, i) => {
     gsap.to(cardEl, {
-      rotate:0,
-      scale:0.3,
-      x: (Math.random() * window.innerWidth) / 1.2 - 100,
-      y: (Math.random() * window.innerHeight) / 1.4,
-      scale:1,
-      force3D:true,
-     
-      ease: "power2.in",
+      rotateY:0, rotateZ:0, rotateX:0,
+      x: window.innerWidth / 2 - 130 + i * -10,
+      y: window.innerHeight / 2 - 300 + i * 20,
+      ease: "back.out",
+      scale: 0.5,
     });
   });
 }
 
-window.addEventListener('resize',resize)
-window.addEventListener('load',resize)
-random.addEventListener("click", cardRandom);
-setting.addEventListener('click',cardSetting)
+function cardRandom() {
+  if (pageNumber === 0)
+    cards.forEach((cardEl, i) => {
+      gsap.to(cardEl, {
+        rotateY:0, rotateZ:0, rotateX:0,
+        scale: 0.3,
+        x: (Math.random() * window.innerWidth) / 1.2 - 100,
+        y: (Math.random() * window.innerHeight) / 1.4,
+        scale: 1,
+        ease: "back2.in",
+      });
+    });
+
+  else if (pageNumber === 1)
+    cards.forEach((cardEl, i) => {
+      gsap.to(cardEl, {
+        rotateZ: Math.floor(Math.random()*160),
+        rotateY:Math.floor(Math.random()*60),
+        x: (Math.random() * window.innerWidth) / 1.2 - 100,
+        y: (Math.random() * window.innerHeight) / 1.4,
+        scale: 1,
+        ease: "back.in",
+      });
+    });
+
+    else if(pageNumber ===2)
+    cards.forEach((cardEl, i) => {
+      gsap.to(cardEl, {
+        perspective:600,
+        rotateZ: Math.floor(Math.random()*160),
+        rotateY:Math.floor(Math.random()*60),
+        x: (Math.random() * window.innerWidth) / 1.2 - 100,
+        y: (Math.random() * window.innerHeight) / 1.4,
+        scale: 1,
+        ease: "back.in",
+      });
+    });
+}
+
+window.addEventListener("resize", resize);
+window.addEventListener("load", resize);
+setting.addEventListener("click", cardSetting);
+
+function resize() {
+  cardSetting();
+}
