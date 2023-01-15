714e34693e5ac1325e4f28623659f3b1dd5cf203
KYW <qodna25@gmail.com>
2023-01-15 11:57:31 +0900

    랜덤카드분사기3

--- 랜덤카드분사기/app.js
+++ 랜덤카드분사기/app.js
@@ -8,6 +8,7 @@ const cards = document.querySelectorAll(".card");
 
 let pageNumber = 0;
 
+//버튼을 포커스하면 해당 버튼에 포커스 효과를 주는 함수
 function pageNumCheck() {
   $button.forEach((btnEl, i) => {
     btnEl.addEventListener("click", () => {
@@ -26,6 +27,7 @@ function pageNumCheck() {
 }
 pageNumCheck();
 
+//카드를 초기 위치로 셋팅해주는 함수
 function cardSetting() {
   cards.forEach((cardEl, i) => {
     gsap.to(cardEl, {
@@ -38,6 +40,7 @@ function cardSetting() {
   });
 }
 
+//카드를 랜덤하게 그려주는 함수
 function cardRandom() {
   if (pageNumber === 0)
     cards.forEach((cardEl, i) => {
@@ -67,20 +70,29 @@ function cardRandom() {
     cards.forEach((cardEl, i) => {
       gsap.to(cardEl, {
         perspective:600,
-        rotateZ: Math.floor(Math.random()*160),
+        rotateZ: Math.floor(Math.random()*267),
         rotateY:Math.floor(Math.random()*60),
         x: (Math.random() * window.innerWidth) / 1.2 - 100,
         y: (Math.random() * window.innerHeight) / 1.4,
-        scale: 1,
-        ease: "back.in",
+        scale: Math.random()*1.1,
+        ease: "expo.in",
       });
     });
 }
 
-window.addEventListener("resize", resize);
-window.addEventListener("load", resize);
+
+//셋팅 버튼 클릭하면 카드 셋팅 함수가 실행
 setting.addEventListener("click", cardSetting);
+
+//윈도우의 화면이 재설정 되면 발생하도록 지정한 이벤트 리스너
+window.addEventListener("resize", resize);
+
+//윈도우가 리로드(재랜더링 되면) 시 실행
+window.addEventListener("load", resize);
 
 function resize() {
   cardSetting();
 }
+
+
+
